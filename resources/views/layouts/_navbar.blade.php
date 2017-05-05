08d94221413b6f38f251013743f5c99618de0a3d
5710404357 <guitarr_gs@hotmail.com>
2017-05-05 16:29:49 +0700

    fix nevbar

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -20,7 +20,7 @@
   								<li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
   								<li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
   								<li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>
-                                <li><a href="#"><i class="fa fa-user"></i> Account</a></li>
+                              
      <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
