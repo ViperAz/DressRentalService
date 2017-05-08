b0c27c497c169772194f91d62f657699ad4e37c1
Unknown <kornbliss@gmail.com>
2017-05-08 14:49:15 +0700

    drop db

--- app/Voucher.php
+++ app/Voucher.php
@@ -7,5 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Voucher extends Model
 {
   protected $fillable = ['code','start_datetime','end_datetime','discounted_price'];
-  protected $visible = ['id','code','start_datetime','end_datetime','discounted_price'];
 }
